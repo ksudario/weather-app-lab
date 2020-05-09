fda59d26247291f8bc6b9cd80ebdc5896c976681
ksudario <60546566+ksudario@users.noreply.github.com>
2020-05-09 18:34:29 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,13 +9,13 @@ const $temp = $('#temp');
 const $wind = $('#wind'); 
 const $input = $('input[type="text"]')
 /*----- event listeners -----*/
-$('form').on('submit', handleGetData)
+$('form').on('submit', handleGetData) //lets user submit form request
 /*----- functions -----*/
 function handleGetData(event) {
-    event.preventDefault(); 
+    event.preventDefault(); //stops the default behavior
     if($input.val()=== "") return; 
    userInput = ($input.val())
-   $input.val(""); //clear input
+   $input.val(""); //clear input 
     $.ajax({
         url: 'http://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=3aa97485a155196e7083ba0223f14371'
     }).then(function(data){
